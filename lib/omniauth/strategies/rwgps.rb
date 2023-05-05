7939b60b3d47f49a04087bde5f2b2b161af0b8c3
Craig <craig@wandrer.earth>
2023-05-05 17:59:51 -0400

    seems good

--- lib/omniauth/strategies/rwgps.rb
+++ lib/omniauth/strategies/rwgps.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'omniauth-oauth2'
-require 'multi_json'
 
 module OmniAuth
   module Strategies
@@ -13,48 +12,11 @@ module OmniAuth
         :token_url => 'https://ridewithgps.com/oauth/token.json'
       }
 
-      def authorize_params
-        super.tap do |params|
-          params[:approval_prompt] = 'force'
-        end
+      uid { raw_info['user']['id'] }
+
+      def raw_info
+        @raw_info ||= access_token.get('/users/current.json').parsed
       end
-
-      def request_phase
-        super
-      end
-
-      def callback_phase
-        super
-      end
-
-      uid { "#{athlete['id']}" }
-
-      extra do
-        {
-          recent_ride_totals: athlete['recent_ride_totals'],
-          ytd_ride_totals: athlete['ytd_ride_totals'],
-          all_ride_totals: athlete['all_ride_totals'],
-          raw_info: athlete
-        }
-      end
-
-      info do
-        {
-          name: "#{athlete['firstname']} #{athlete['lastname']}",
-          first_name: athlete['firstname'],
-          last_name: athlete['lastname'],
-          email: athlete['email'],
-          location: "#{athlete['city']} #{athlete['state']}",
-          image: athlete['profile']
-        }
-      end
-
-      def athlete
-        access_token.options[:mode] = :query
-        access_token.options[:param_name] = :access_token
-        @athlete ||= MultiJson.load(access_token.get('/api/v3/athlete', { access_token: access_token.token }).body)
-      end
-
     end
   end
 end
